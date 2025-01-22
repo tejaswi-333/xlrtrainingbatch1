ed39b57981371768434feb3b9774526a8cfe311d
tejaswi-333 <tejaswim369@gmail.com>
2025-01-22 20:18:16 +0530

    printing name, age and phone number

--- core java/Demo007.java
+++ core java/Demo007.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Demo7 {
+public class Demo007 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Username: ");
