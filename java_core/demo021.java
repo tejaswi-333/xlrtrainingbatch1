977e1d5af7e976340ff4cb41a2c4235c99aea5f3
tejaswi-333 <tejaswim369@gmail.com>
2025-01-23 11:27:16 +0530

    leap year

--- java_core/demo021.java
+++ java_core/demo021.java
@@ -1,5 +1,5 @@
 import  java.util.Scanner;
-public class Demo022 {
+public class demo021 {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter the year: ");
