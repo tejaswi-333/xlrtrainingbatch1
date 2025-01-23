cd9bdbe0ee252c704aed01d6358360ef35ebcd8c
tejaswi-333 <tejaswim369@gmail.com>
2025-01-23 10:26:31 +0530

    print 1 to 100 numbers

--- java_core/demo004.java
+++ java_core/demo004.java
@@ -1,6 +1,6 @@
 class demo004 {
     public static void main(String[] args) {
-        //print 1 to 100 numbers
+        //print 1 to 100 numbers.
         for(int i=1;i<101;i++){
             System.out.println(i);
         }
