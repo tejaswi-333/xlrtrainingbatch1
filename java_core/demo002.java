5eb2847fdd940d8127e1b37c5c9fa6f877dd9178
tejaswi-333 <tejaswim369@gmail.com>
2025-01-23 10:21:23 +0530

    greatest number of 3

--- java_core/demo002.java
+++ java_core/demo002.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class demo002 {
     public static void main(String[] args) {
-        //greatest number of 3
+        //greatest number of 3.
         
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of First number: ");
