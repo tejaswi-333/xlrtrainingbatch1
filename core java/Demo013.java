59901c1c45817e43e2a58e93159e9c7d1a1b0644
tejaswi-333 <tejaswim369@gmail.com>
2025-01-23 10:08:51 +0530

    Delete core java directory

--- core java/Demo013.java
+++
@@ -1,9 +0,0 @@
-public class Demo013 {
-    public static void main(String[] args) {
-        double height=8;
-        double base=5;
-        double area= 0.5*base*height;
-
-        System.out.println("Area of triangle is "+area);
-    }
-}
