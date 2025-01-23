08006a3d837bf15ae14f51e5b2eb1c528babcdbd
tejaswi-333 <tejaswim369@gmail.com>
2025-01-23 11:06:52 +0530

    Friends list

--- java_core/demo025.java
+++ java_core/demo025.java
@@ -1,6 +1,6 @@
 public class demo025 {
     public static void main(String[] args) {
-        String[] f1 = {"Mani ","Srujan ","Tarak ","venu ","vinay "};
+        String[] f1 = {"Harsha ","Nandu ","Priya ","Kavita ","Tharuni "};
         String[] address= {"Hyd ", "KPHB ","KPHB ","Medak ","Medak "};
         long[] number = {70324443,24890112,987839991,776891011,1090775432};
 
