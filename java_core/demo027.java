f7ac3b4fd8b4550a864235ec76ea3325af788b03
tejaswi-333 <tejaswim369@gmail.com>
2025-01-23 11:08:39 +0530

    is prime or not

--- java_core/demo027.java
+++ java_core/demo027.java
@@ -4,7 +4,7 @@ class demo027{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int n = sc.nextInt();
-        //check whether entered number is prime or not
+        //check whether entered number is prime or not.
         for(int i = 2; i<=n/2 ; i++){
             if(n%i==0){
                 System.out.println(n + " is not a prime number");
