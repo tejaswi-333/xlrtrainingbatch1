9a7ac5a2b8dd0790c78b740ade68fd9f7d117a7d
tejaswi-333 <tejaswim369@gmail.com>
2025-01-23 10:27:38 +0530

    admin login using while

--- java_core/demo005.java
+++ java_core/demo005.java
@@ -3,7 +3,7 @@ class demo005 {
     public static void main(String[] args) {
         String Username = "Admin";
         String Password = "Admin_001";
-        //admin login using while
+        //admin login using while.
 
         while((Username == "Admin") && (Password == "Admin_001")) {
             System.out.println("Hello User");
