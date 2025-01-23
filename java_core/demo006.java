5ef730fb956fd153bb790e32e030d3132da714d9
tejaswi-333 <tejaswim369@gmail.com>
2025-01-23 10:30:36 +0530

    User login using while loop

--- java_core/demo006.java
+++ java_core/demo006.java
@@ -2,10 +2,10 @@
 class demo006 {
     public static void main(String[] args) {
 
-        String Username = "Srujan";
-        String Password = "Srujan_123";
+        String Username = "Tejaswi";
+        String Password = "unknown";
     //User login using while loop
-        while((Username == "Srujan") && (Password == "Srujan_123")) {
+        while((Username == "Tejaswi") && (Password == "unknown")) {
             System.out.println("Welcome to Mallareddy university");
             break;
         }
