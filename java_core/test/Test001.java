cdc951a19d179a839539208cc83c1c107e5826e3
tejaswi-333 <tejaswim369@gmail.com>
2025-01-26 15:51:24 +0530

    print digits 1 to 10 except 5 and 7

--- java_core/test/Test001.java
+++ java_core/test/Test001.java
@@ -1,4 +1,5 @@
-public class test001{
+public class Test001{
+    //print digits 1 to 10 except 5 and 7
     public static void main(String[] args) {
         for(int i=0; i<=10; i++){
             if(i==5 || i==7){
