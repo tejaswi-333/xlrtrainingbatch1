00eeca42303cfd025dcf846537a085022a521906
tejaswi-333 <tejaswim369@gmail.com>
2025-01-26 15:37:37 +0530

    Swap the numbers using a third variable

--- java_core/Demo044.java
+++ java_core/Demo044.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo044 {
+public class Demo044 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the first number: ");
