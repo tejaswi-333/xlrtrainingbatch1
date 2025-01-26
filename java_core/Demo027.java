ceb03f5c2dc67765f2c82a36cfe6bc509741c410
tejaswi-333 <tejaswim369@gmail.com>
2025-01-26 15:26:25 +0530

    prime or not.

--- java_core/Demo027.java
+++ java_core/Demo027.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class demo027{
+class Demo027{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
