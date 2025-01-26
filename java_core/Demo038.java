40aaf62bb52035b6e3c36883a52d5c86ca64d456
tejaswi-333 <tejaswim369@gmail.com>
2025-01-26 15:34:36 +0530

    lowercase and uppercase

--- java_core/Demo038.java
+++ java_core/Demo038.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 //lowercase and uppercase
-public class demo038 {
+public class Demo038 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a character: ");
