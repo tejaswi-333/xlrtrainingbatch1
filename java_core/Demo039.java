de62ae20c8fbf27143e63ea86cc15f9050381eb2
tejaswi-333 <tejaswim369@gmail.com>
2025-01-26 15:35:03 +0530

    lowercase or uppercase or digit

--- java_core/Demo039.java
+++ java_core/Demo039.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo039 {
+public class Demo039 {
     //Write a java program to check character lowercase or uppercase or digit
     public static void main (String args[]){
         Scanner sc=new Scanner(System.in);
