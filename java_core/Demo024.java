ac804eb7737c6fca13dbd2153afbd0287840ef3e
tejaswi-333 <tejaswim369@gmail.com>
2025-01-26 15:21:22 +0530

    Reversed String

--- java_core/Demo024.java
+++ java_core/Demo024.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 //Reversed String
-class demo024{
+class Demo024{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String:  ");
