72b1f6fe2124c1ac801637d5d48b1be56f9c9829
tejaswi-333 <tejaswim369@gmail.com>
2025-01-26 15:38:58 +0530

    Count the number of digits

--- java_core/Demo047.java
+++ java_core/Demo047.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo047 {
+public class Demo047 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
