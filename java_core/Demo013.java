b8b1ddf2eab4e2055eff697f1ba403d2c6700f0f
tejaswi-333 <tejaswim369@gmail.com>
2025-01-26 14:38:40 +0530

    Area of the triangle

--- java_core/Demo013.java
+++ java_core/Demo013.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 //Area of the triangle
-public class demo013 {
+public class Demo013 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
