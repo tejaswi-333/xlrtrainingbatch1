4b51b11fcfec4537eb5a579e71b70c57d36cc3fc
tejaswi-333 <tejaswim369@gmail.com>
2025-01-26 15:25:44 +0530

    number of vowels and consonants in a string.

--- java_core/Demo026.java
+++ java_core/Demo026.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class demo026{
+class Demo026{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the String: ");
