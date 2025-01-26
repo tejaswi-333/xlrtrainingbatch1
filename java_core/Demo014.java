10374f608603789b0e88ec3dd48cf40155206554
tejaswi-333 <tejaswim369@gmail.com>
2025-01-26 14:39:06 +0530

    unary operators.

--- java_core/Demo014.java
+++ java_core/Demo014.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class demo014 {
+public class Demo014 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number: ");
