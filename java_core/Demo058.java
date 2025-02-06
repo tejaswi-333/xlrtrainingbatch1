a3fa4cc574bb8aca215fdd7e794de8eec2dade10
tejaswi-333 <tejaswim369@gmail.com>
2025-02-06 11:11:55 +0530

    factors of number

--- java_core/Demo058.java
+++ java_core/Demo058.java
@@ -1,7 +1,7 @@
 public class Demo058 {
     public static void main(String[] args) {
         int x = 14;
-
+//Write a java program to print factors of number
         System.out.print("Factors of "+x+" are: ");
         for(int i=1; i<=x; i++){
             if(x%i==0){
