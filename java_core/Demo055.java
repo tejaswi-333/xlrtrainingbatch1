80b0f3ae284c3b3a14d414c70d7577e67e1f2087
tejaswi-333 <tejaswim369@gmail.com>
2025-02-06 11:09:35 +0530

    sum of squares of a digit of a number

--- java_core/Demo055.java
+++ java_core/Demo055.java
@@ -1,7 +1,7 @@
 public class Demo055 {
     static void squareSum(int x){
         int sum = 0;
-
+//Write a java program to print sum of squares of a digit of a number.
         while(x!=0){
             int digit = x%10;
             sum = sum + (digit*digit);
