f1a59532b8f3afa36b01871877a32199821ac60f
tejaswi-333 <tejaswim369@gmail.com>
2025-02-07 11:32:12 +0530

    print index values of an array

--- java_core/Demo066.java
+++ java_core/Demo066.java
@@ -1,5 +1,5 @@
 public class Demo066 {
-    //Write a java program to print index values of an array
+    //Write a java program to print index values of an array.
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50};  
         System.out.println("Index | Value");
