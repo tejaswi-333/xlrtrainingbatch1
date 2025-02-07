01c304b4a228b282c424f9aa1b482e1e5af2022b
tejaswi-333 <tejaswim369@gmail.com>
2025-02-07 11:39:22 +0530

    sum of all the elements of an array

--- java_core/Demo069.java
+++ java_core/Demo069.java
@@ -1,5 +1,5 @@
 public class Demo069 {
-    //Write a java program to print sum of all the elements of an array.
+    //Write a java program to print sum of all the elements of an array
     public static void main(String[] args) {
         int[] arr = {10, 20, 30, 40, 50}; 
         int sum = 0;
