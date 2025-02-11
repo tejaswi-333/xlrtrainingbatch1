78aec21e32d4c0fdbd1b694dd91650baf688236d
tejaswi-333 <tejaswim369@gmail.com>
2025-02-11 11:40:12 +0530

    Add files via upload

---
+++ java_core/Demo067.java
@@ -0,0 +1,16 @@
+class PrintEvenElements{
+    void evenElements(int[] arr){
+        int[] even = arr;
+        for (int i = 0; i< even.length; i++){
+            if (even[i] % 2 == 0)
+            System.out.print(even[i]+" ");
+        } 
+    }
+}
+public class Demo067 {
+    public static void main(String[] args) {
+        PrintEvenElements obj = new PrintEvenElements();
+        int[] arr = {10,50,89,90,23};
+        obj.evenElements(arr);
+    }
+}
